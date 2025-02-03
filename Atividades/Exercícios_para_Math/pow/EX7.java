1d4772453a30add7bb036ea1e6b25d620b307fd8
Gabriel Arthur <gbl48287@gmail.com>
2025-02-03 19:51:50 -0300

    Subindo

--- Atividades/Exercícios_para_Math/pow/EX7.java
+++ Atividades/Exercícios_para_Math/pow/EX7.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class EX7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println(Math.pow(0.5,2));
     }
 }
