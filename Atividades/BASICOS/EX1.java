04f14b595f327c0c08b25f0b7870ee720ead9875
Gabriel Arthur <gbl48287@gmail.com>
2025-02-06 19:54:21 -0300

    Atividade_up01

--- Atividades/BASICOS/EX1.java
+++ Atividades/BASICOS/EX1.java
@@ -6,7 +6,7 @@ public class EX1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
-        System.out.printf("Digite um numero: ");
+        System.out.println("Digite um numero: ");
         n = sc.nextInt();
 
         if(n > 0){
